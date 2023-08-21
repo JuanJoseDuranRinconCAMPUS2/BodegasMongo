403f153a9cd4f9534381071778a9b324a23e7f92
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-08-21 10:31:41 -0500

    feat: :construction: Implementacion de "proxyEndpointVerify"en los endpoints: GetBodegas
    Se modifica el archivo para incluir el Middleware de validacion de "proxyEndpointVerify", validando que el token contenga la informacion necesaria para ingresar al endpoint. Ya sea el endpoint como tal y el rol si es necesario

--- backend/routes/GetBodegas.js
+++ backend/routes/GetBodegas.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { limitGColecciones } from "../limit/limit.js";
+import { proxyEndpointVerify } from "../middleware/proxyManejoTokens.js";
 import { con } from '../db/atlas.js';
 
 const AppGBodegas = Router();
 let db = await con();
 
-AppGBodegas.get('/', limitGColecciones(), async (req, res) =>{
+AppGBodegas.get('/', limitGColecciones(), proxyEndpointVerify(1, "ListarBodegas", "Admin"), async (req, res) =>{
     if(!req.rateLimit) return;
     let alquiler = db.collection("bodegas");
     let result = await alquiler.find({}).sort( { nombre: 1  } ).toArray();
